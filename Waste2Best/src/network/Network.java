e03d31d612c6fee2fa8b8e840c1ed35b850487c8
RajashreeJoshi <joshi.raj@husky.neu.edu>
2019-04-10 21:36:40 -0400

    Organization & Enterprize Package

--- Waste2Best/src/network/Network.java
+++ Waste2Best/src/network/Network.java
@@ -4,12 +4,12 @@
  * and open the template in the editor.
  */
 
-package business.models;
+package network;
 
 /**
  *
  * @author Rajashree
  */
-public class Lab {
-    
+public class Network {
+
 }
