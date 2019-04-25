40be0d27bdc15e3e5f4cd85957cb850cdd0dbc2c
Aditya <kamat.a@husky.neu.edu>
2019-04-25 13:55:35 -0400

    compost purchase request

---
+++ Waste2Best/src/business/models/workRequest/PurchaseCompostWorkRequest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.models.workRequest;
+
+/**
+ *
+ * @author Aditya
+ */
+public class PurchaseCompostWorkRequest extends WorkRequest {
+    private String type;
+    private int quantity;
+    private String productId;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    
+}
